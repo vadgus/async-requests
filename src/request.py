7fa7e17617ff3a4d20abd66ffbe0fac8b3d4b9b9
Vadim Gusyatnikov <guva@gurtam.com>
2019-11-30 15:08:51 +0300

    argument validation: single return

--- src/request.py
+++ src/request.py
@@ -10,27 +10,22 @@ class Request:
     _tasks = []
     _read = 0
 
-    def run(self, amount: int):
-        if not isinstance(amount, int):
-            print("Argument must be type int, not {type(amount)}")
-            return False
-        if not amount > 0:
-            print("Argument must be more than zero")
-            return False
+    def run(self, amount: int) -> bool:
+        if isinstance(amount, int) and amount > 0:
+            self._amount = amount
+            loop = asyncio.get_event_loop()
 
-        self._amount = amount
-        loop = asyncio.get_event_loop()
+            url = "https://www.google.com/?hash="
+            for i in range(self._amount):
+                url = url + "1"
+                task = asyncio.ensure_future(self._request(url.format(i), i))
+                self._tasks.append(task)
 
-        url = "https://www.google.com/?hash="
-        for i in range(self._amount):
-            url = url + "1"
-            task = asyncio.ensure_future(self._request(url.format(i), i))
-            self._tasks.append(task)
+            self._start = time()
+            loop.run_until_complete(asyncio.wait(self._tasks))
 
-        self._start = time()
-        loop.run_until_complete(asyncio.wait(self._tasks))
+            print("\n%s seconds" % str(time() - self._start))
 
-        print("\n%s seconds" % str(time() - self._start))
         return self._amount == self._read
 
     async def _request(self, url, index):
@@ -41,4 +36,3 @@ class Request:
                 await response.read()
                 self._read += 1
                 print('%d: %f' % (index, time()))
-                return True
